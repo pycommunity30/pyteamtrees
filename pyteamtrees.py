4ec57d0738673280c0bc29ca437e2b037f90e524
delta2020 <zi.azzr227@gmail.com>
2020-09-01 10:55:31 -0600

    PEP8 Changes

--- pyteamtrees.py
+++ pyteamtrees.py
@@ -4,12 +4,12 @@ import random
 import time
 x = 100
 y = 100
-treex = random.randint(20,580)
-treey = random.randint(40,380)
-window = pygame.display.set_mode((600,400),0,32)
-green = (0,150,0)
-brown = (50,40,0)
-white = (255,255,255)
+treex = random.randint(20, 580)
+treey = random.randint(40, 380)
+window = pygame.display.set_mode((600, 400), 0, 32)
+green = (0, 150, 0)
+brown = (50, 40, 0)
+white = (255, 255, 255)
 mission = "Collect the saplings!!!"
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
@@ -35,10 +35,9 @@ while True:
     elif move == "right":
         x += 30
 
-
     window.fill(white)
     textsurface = myfont.render("Mission: " + mission, False, (0, 0, 0))
     window.blit(textsurface, (0, 0))
-    pygame.draw.rect(window, brown,((treex + 9),(treey + 30),15,30))
-    pygame.draw.rect(window, green,(treex,treey,30,30))
+    pygame.draw.rect(window, brown, ((treex + 9), (treey + 30), 15, 30))
+    pygame.draw.rect(window, green, (treex, treey, 30, 30))
     pygame.display.update()
